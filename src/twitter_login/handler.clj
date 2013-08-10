4a2b38d593429f8106a01509086b552195fe569b
John Cowie <john.a.cowie@gmail.com>
2013-08-10 17:38:51 +0100

    Got twitter login working

--- src/twitter_login/handler.clj
+++ src/twitter_login/handler.clj
@@ -6,22 +6,40 @@
    [ring.middleware.session :as session]
    [ring.util.response :refer [response redirect content-type]]
    [hiccup.core :refer [html]]
-   ))
+   )
+  (:import [twitter4j Twitter TwitterFactory]
+           [twitter4j.conf PropertyConfiguration]))
 
 (defn login-page []
   (content-type (response (html
                            [:form {:method "post"}
-                            [:input {:name "name" :size 30}]
-                            [:input {:type "submit" :name "submit" :value "submit"}]
+                            [:input {:type "submit" :value "login"}]
                             ]
                            ))  "text/html"
                 ))
 
-(defn login [{name :name} redirect-url]
-  (assoc
-      (redirect redirect-url)
-    :session {:name name}
-   ))
+(def twitter-config
+  (PropertyConfiguration. (clojure.java.io/input-stream "/Users/John/Dropbox/nuotltester.properties")))
+
+(defn login [redirect-url]
+  (let [twitter (. (TwitterFactory. twitter-config) (getInstance))
+        callback-url "http://localhost:7777/callback"
+        request-token (. twitter (getOAuthRequestToken callback-url))]
+    (assoc
+        (redirect (. request-token (getAuthenticationURL)))
+      :session {:twitter twitter :request-token request-token}
+      )))
+
+(defn callback [session params]
+  (let [twitter (:twitter session)
+        request-token (:request-token session)
+        verifier (:oauth_verifier params)]
+    (. twitter (getOAuthAccessToken request-token verifier))
+    (assoc
+        (redirect "/")
+      :session {:name (. twitter (getScreenName))}
+      )
+    ))
 
 (defn logout [redirect-url]
   (assoc
@@ -42,8 +60,9 @@
 (defroutes app-routes
   (GET "/" {session :session} (home session "/login"))
   (GET "/login" [] (login-page))
-  (POST "/login" {params :params} (login params "/"))
+  (POST "/login" {params :params} (login "/"))
   (POST "/logout" []  (logout "/"))
+  (GET "/callback" {session :session params :params} (callback session params))
   (route/resources "/")
   (route/not-found "Not Found"))
 
