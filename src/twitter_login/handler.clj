75044fc2cee0502ca79da94ef24a6f8fc4964448
John Cowie <john.a.cowie@gmail.com>
2013-08-10 16:31:18 +0100

    Got basic login with session cookies working

--- src/twitter_login/handler.clj
+++ src/twitter_login/handler.clj
@@ -1,12 +1,53 @@
 (ns twitter-login.handler
-  (:use compojure.core)
-  (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+  (:require
+   [compojure.core :refer [defroutes GET POST]]
+   [compojure.handler :as handler]
+   [compojure.route :as route]
+   [ring.middleware.session :as session]
+   [ring.util.response :refer [response redirect content-type]]
+   [hiccup.core :refer [html]]
+   ))
+
+(defn login-page []
+  (content-type (response (html
+                           [:form {:method "post"}
+                            [:input {:name "name" :size 30}]
+                            [:input {:type "submit" :name "submit" :value "submit"}]
+                            ]
+                           ))  "text/html"
+                ))
+
+(defn login [{name :name} redirect-url]
+  (assoc
+      (redirect redirect-url)
+    :session {:name name}
+   ))
+
+(defn logout [redirect-url]
+  (assoc
+   (redirect redirect-url)
+   :cookies {"ring-session" {:value "" :max-age 0}}
+   ))
+
+(defn home [{name :name} login-url]
+  (if (nil? name)
+    (redirect login-url)
+    (content-type (response (html
+                             [:h2 (format "Hello %s" name)]
+                             [:form {:method "post" :action "/logout"}
+                              [:input {:type "submit" :value "logout"}]
+                              ]
+                             )) "text/html")))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" {session :session} (home session "/login"))
+  (GET "/login" [] (login-page))
+  (POST "/login" {params :params} (login params "/"))
+  (POST "/logout" []  (logout "/"))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (-> (handler/site app-routes)
+      (session/wrap-session)
+      ))
